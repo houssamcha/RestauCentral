c0f62ffa0aa199b69927ed241715f5b967ac0aec
houssamcha <chagourihoussam@gmail.com>
2024-08-25 00:43:58 +0100

    Creation du package Service

--- src/main/java/com/RestauCentral/RestauCentral/Repository/RestaurantRepository.java
+++ src/main/java/com/RestauCentral/RestauCentral/Repository/RestaurantRepository.java
@@ -4,5 +4,5 @@ import com.RestauCentral.RestauCentral.Model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
-    
+
 }
