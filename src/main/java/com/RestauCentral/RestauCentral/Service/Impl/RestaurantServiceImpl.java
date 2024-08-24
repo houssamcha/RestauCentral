188efb90c649683c6de514ebe5c09e1fa4bd2d8b
houssamcha <chagourihoussam@gmail.com>
2024-08-25 00:49:43 +0100

    Annotation @Service

--- src/main/java/com/RestauCentral/RestauCentral/Service/Impl/RestaurantServiceImpl.java
+++ src/main/java/com/RestauCentral/RestauCentral/Service/Impl/RestaurantServiceImpl.java
@@ -1,7 +1,9 @@
 package com.RestauCentral.RestauCentral.Service.Impl;
 
 import com.RestauCentral.RestauCentral.Service.RestaurantService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RestaurantServiceImpl implements RestaurantService {
-
+    
 }
