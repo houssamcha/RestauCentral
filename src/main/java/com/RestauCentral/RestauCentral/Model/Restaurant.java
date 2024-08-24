edc78f0aa6db0fa25c8fe892546c18d95a5c532c
houssamcha <chagourihoussam@gmail.com>
2024-08-24 23:42:26 +0100

    Modification dans les annotations dans la class Restaurant

--- src/main/java/com/RestauCentral/RestauCentral/Model/Restaurant.java
+++ src/main/java/com/RestauCentral/RestauCentral/Model/Restaurant.java
@@ -2,16 +2,21 @@ package com.RestauCentral.RestauCentral.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Restaurant {
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String address;
