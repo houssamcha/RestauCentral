5bee80b1420782f5f4f425bd9f2a71c95ec45a21
houssamcha <chagourihoussam@gmail.com>
2024-08-25 00:42:50 +0100

    Creation du package Repository et de la class RestaurantRepository

---
+++ src/main/java/com/RestauCentral/RestauCentral/Repository/RestaurantRepository.java
@@ -0,0 +1,8 @@
+package com.RestauCentral.RestauCentral.Repository;
+
+import com.RestauCentral.RestauCentral.Model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
+    
+}
