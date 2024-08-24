160c3ed3be44b3a990c036913e435bf8d78a833f
houssamcha <chagourihoussam@gmail.com>
2024-08-24 23:12:09 +0100

    Création du package Model

---
+++ src/main/java/com/RestauCentral/RestauCentral/RestauCentralApplication.java
@@ -0,0 +1,13 @@
+package com.RestauCentral.RestauCentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauCentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RestauCentral/RestauCentral/RestauCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestauCentral.RestauCentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
