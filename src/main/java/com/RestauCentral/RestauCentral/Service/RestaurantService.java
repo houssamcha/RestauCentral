1d6870eba9974d3700d3741da16f7149a07aa28d
houssamcha <chagourihoussam@gmail.com>
2024-09-08 02:37:49 +0100

    Définir les méthodes de l'interface service pour l'entité Restaurant

--- src/main/java/com/RestauCentral/RestauCentral/Service/RestaurantService.java
+++ src/main/java/com/RestauCentral/RestauCentral/Service/RestaurantService.java
@@ -1,4 +1,12 @@
 package com.RestauCentral.RestauCentral.Service;
 
+import com.RestauCentral.RestauCentral.Model.Restaurant;
+import java.util.List;
+
 public interface RestaurantService {
+    List<Restaurant> getAllRestaurants();
+    Restaurant getRestaurantById(int id);
+    Restaurant addRestaurant(Restaurant restaurant);
+    void deleteRestaurant(int id);
+    Restaurant updateRestaurant(int id, Restaurant restaurant);
 }
