5181a45c9dbc711efc86931b55ef24769d3b75d8
houssamcha <chagourihoussam@gmail.com>
2024-09-08 02:34:13 +0100

    Creation du package Controller and class RestaurantController

---
+++ src/main/java/com/RestauCentral/RestauCentral/Controller/RestaurantController.java
@@ -0,0 +1,9 @@
+package com.RestauCentral.RestauCentral.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class RestaurantController {
+}
