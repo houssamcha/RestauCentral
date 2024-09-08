2c088ff8b8e7fb77552dd8e7a6c654548887b5d4
houssamcha <chagourihoussam@gmail.com>
2024-09-08 04:00:50 +0100

    Ajout de HttpClientModule dans app.module.ts pour permettre les appels API

--- restau-central-frontend/src/app/app.module.ts
+++ restau-central-frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { RestaurantListComponent } from './restaurant-list/restaurant-list.compo
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- restau-central-frontend/src/app/restaurant-list/restaurant-list.component.ts
+++ restau-central-frontend/src/app/restaurant-list/restaurant-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RestaurantService } from '../services/restaurant.services';
+import { Restaurant } from '../Model/restaurant.model';
 
 @Component({
   selector: 'app-restaurant-list',
   templateUrl: './restaurant-list.component.html',
   styleUrls: ['./restaurant-list.component.css']
 })
-export class RestaurantListComponent {
+export class RestaurantListComponent implements OnInit {
+  restaurants: Restaurant[] = [];
 
+  constructor(private restaurantService: RestaurantService) { }
+
+  ngOnInit(): void {
+    this.restaurantService.getRestaurants().subscribe(
+      (data: Restaurant[]) => this.restaurants = data,
+      (error) => console.error('Error fetching restaurants', error)
+    );
+  }
 }
