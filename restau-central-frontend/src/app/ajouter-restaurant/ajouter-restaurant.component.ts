889f6118af53b8af263182ca327bdb721a8b1739
houssamcha <chagourihoussam@gmail.com>
2024-09-09 23:18:57 +0100

    Implémentation de la méthode addRestaurant dans ajouter-restaurant.component.ts pour envoyer les données du restaurant au backend via le service Angular

--- restau-central-frontend/src/app/ajouter-restaurant/ajouter-restaurant.component.ts
+++ restau-central-frontend/src/app/ajouter-restaurant/ajouter-restaurant.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { RestaurantService } from 'src/app/services/restaurant.services';
+import { Restaurant } from 'src/app/Model/restaurant.model';
 
 @Component({
-  selector: 'app-ajouter-restaurant',
+  selector: 'app-restaurant-add',
   templateUrl: './ajouter-restaurant.component.html',
   styleUrls: ['./ajouter-restaurant.component.css']
 })
-export class AjouterRestaurantComponent {
+export class RestaurantAddComponent {
+  restaurant: Restaurant = {
+    name: '',
+    address: '',
+    phone: '',
+    commandes: '',
+    serviceLivraison: ''
+  };
 
+  constructor(private restaurantService: RestaurantService) {}
+
+  // The function triggered when the form is submitted
+  onSubmit() {
+    this.restaurantService.addRestaurant(this.restaurant).subscribe(
+      (response) => {
+        console.log('Restaurant ajouté avec succès !', response);
+        // Reset form or redirect user as needed
+        this.restaurant = {
+          name: '',
+          address: '',
+          phone: '',
+          commandes: '',
+          serviceLivraison: ''
+        };
+      },
+      (error) => {
+        console.error('Erreur lors de l\'ajout du restaurant', error);
+      }
+    );
+  }
 }
