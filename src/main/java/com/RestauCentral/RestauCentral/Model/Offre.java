a9fbac1f18226d65b697680d3a6ba3b6c89df14a
houssamcha <chagourihoussam@gmail.com>
2024-09-20 18:32:05 +0100

    creation des attributs de l'offre et la relation ManyToOne avec Restaurant

--- src/main/java/com/RestauCentral/RestauCentral/Model/Offre.java
+++ src/main/java/com/RestauCentral/RestauCentral/Model/Offre.java
@@ -1,5 +1,27 @@
 package com.RestauCentral.RestauCentral.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Offre {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String description;
+    private Double prix;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+
 }
