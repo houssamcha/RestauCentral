98d8bb2753af3675d747e27f8b43f1b08807633a
houssamcha <chagourihoussam@gmail.com>
2024-09-21 01:11:46 +0100

    Creation de l'offre Controller dans le backend

--- restau-central-frontend/src/app/offre-list/offre-list.component.ts
+++ restau-central-frontend/src/app/offre-list/offre-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-offre-list',
   templateUrl: './offre-list.component.html',
   styleUrls: ['./offre-list.component.css']
 })
-export class OffreListComponent {
 
+export class OffreListComponent implements OnInit {
+  restaurantId: number;
+  offres: Offre[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private offreService: OffreService
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.restaurantId = +params['id'];
+      this.loadOffres();
+    });
+  }
+
+  loadOffres() {
+    this.offreService.getOffresByRestaurant(this.restaurantId)
+      .subscribe(offres => this.offres = offres);
+  }
 }
---
+++ src/main/java/com/RestauCentral/RestauCentral/Controller/OffreController.java
@@ -0,0 +1,64 @@
+package com.RestauCentral.RestauCentral.Controller;
+
+import com.RestauCentral.RestauCentral.Model.Offre;
+import com.RestauCentral.RestauCentral.Service.OffreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/offres")
+public class OffreController {
+
+    @Autowired
+    private OffreService offreService;
+
+    // Get all offers
+    @GetMapping
+    public List<Offre> getAllOffres() {
+        return offreService.getAllOffres();
+    }
+
+    // Get an offer by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Offre> getOffreById(@PathVariable Long id) {
+        Offre offre = offreService.getOffreById(id);
+        if (offre != null) {
+            return ResponseEntity.ok(offre);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Get offers by restaurant ID
+    @GetMapping("/restaurant/{restaurantId}")
+    public List<Offre> getOffresByRestaurantId(@PathVariable Long restaurantId) {
+        return offreService.getOffresByRestaurantId(restaurantId);
+    }
+
+    // Create a new offer
+    @PostMapping
+    public Offre addOffre(@RequestBody Offre offre) {
+        return offreService.addOffre(offre);
+    }
+
+    // Update an offer by ID
+    @PutMapping("/{id}")
+    public ResponseEntity<Offre> updateOffre(@PathVariable Long id, @RequestBody Offre offre) {
+        Offre updatedOffre = offreService.updateOffre(id, offre);
+        if (updatedOffre != null) {
+            return ResponseEntity.ok(updatedOffre);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Delete an offer by ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOffre(@PathVariable Long id) {
+        offreService.deleteOffre(id);
+        return ResponseEntity.noContent().build();
+    }
+}
