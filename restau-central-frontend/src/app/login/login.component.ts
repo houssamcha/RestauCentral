40512927e9d8e37ff45af49f5bea2667b6af6d1e
houssamcha <chagourihoussam@gmail.com>
2024-10-01 16:44:20 +0100

    Creation du logic de login dans le login.component.ts

--- restau-central-frontend/src/app/login/login.component.ts
+++ restau-central-frontend/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service'; // Adjust the path as needed
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (response) => {
+        // Handle successful login
+        console.log('Login successful', response);
+        this.router.navigate(['/home']); // Redirect to a home or dashboard route
+      },
+      error: (error) => {
+        // Handle error
+        console.error('Login failed', error);
+      }
+    });
+  }
 }
