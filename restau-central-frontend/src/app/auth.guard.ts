b4a466ad4c2e575318d0dea123c29dee1a26201d
houssamcha <chagourihoussam@gmail.com>
2024-10-03 10:30:35 +0100

    Implémentation du Route Guard

--- restau-central-frontend/src/app/auth.guard.ts
+++ restau-central-frontend/src/app/auth.guard.ts
@@ -1,5 +1,21 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    if (this.authService.isLoggedIn()) {
+      return true; // L'utilisateur est authentifié
+    } else {
+      this.router.navigate(['/login']); // Rediriger vers le login
+      return false; // L'accès est refusé
+    }
+  }
+}
